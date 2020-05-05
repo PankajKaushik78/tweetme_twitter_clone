99f4c89b2d0132de1d5b4afe6915924ad5955acc
Pankaj Kaushik <kaushikpankaj78@gmail.com>
2020-05-05 15:56:36 +0530

    Modified admi area

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Tweet
 
-# Register your models here.
-admin.site.register(Tweet)
+
+class TweetAdmin(admin.ModelAdmin):
+    search_fields = ["user__username", "user__email"]
+    list_display = ["__str__", "user"]
+
+    class Meta:
+        model = Tweet
+
+
+admin.site.register(Tweet, TweetAdmin)
